eaa760588efe0cc2093931c83c0992d808dc5efe
Jay Mehta <jaymmehta97@gmail.com>
2020-07-25 14:02:59 +0530

    Changes to core-data manager and option to change store items
    - Fixed bug where only the title of menu was copied rather than the content
    - Abitlity to change store items value in general preferences
    - Changed window title to appName
    - Changed CoreDataManager to support delegate subscribes for specific entities
    - Added alert when store items is changed to lower than currently saved items.

--- Copy Keep/Controllers/ViewControllers/GeneralPreferencesViewController.swift
+++ Copy Keep/Controllers/ViewControllers/GeneralPreferencesViewController.swift
@@ -18,7 +18,6 @@ class GeneralPreferencesViewController: NSViewController {
 
     // MARK: - Properties
 
-    private var copyItems = [CopyItem]()
     private let generalPreferencesVM = GeneralPreferencesViewModel()
 
     // MARK: - Lifecycle Methods
@@ -55,11 +54,16 @@ extension GeneralPreferencesViewController {
     }
 
     private func setup() {
-        copyItems = generalPreferencesVM.getCopyItems() ?? []
+        generalPreferencesVM.fetchCopyItems()
+
         setupCopiedTableView()
         setupDeleteItemsButton(forSelectedItems: 0)
 
-        CoreDataManager.shared.addDelegate(coreDataManagerDelegate: self)
+        numberOfItemToStoreTextField.stringValue = Constants.storeItems.description
+        numberOfItemToStoreTextField.delegate = self
+
+        // Add GeneralPreferencesVC to CoreDataManager's delegate list to receive database changes events
+        CoreDataManager.shared.addDelegate(coreDataManagerDelegate: self, forEntity: .copyItem)
     }
 }
 
@@ -67,7 +71,7 @@ extension GeneralPreferencesViewController: NSTableViewDataSource {
     // MARK: - NSTableViewDataSource methods
 
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return copyItems.count
+        return generalPreferencesVM.copyItems.count
     }
 }
 
@@ -78,8 +82,8 @@ extension GeneralPreferencesViewController: NSTableViewDelegate {
         guard let cell = copiedItemsTableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: generalPreferencesVM.copiedItemsTableViewCellId), owner: nil) as? NSTableCellView else {
             return nil
         }
-        cell.textField?.stringValue = copyItems[row].content ?? ""
-        cell.toolTip = copyItems[row].content ?? ""
+        cell.textField?.stringValue = generalPreferencesVM.copyItems[row].content ?? ""
+        cell.toolTip = generalPreferencesVM.copyItems[row].content ?? ""
         return cell
     }
 }
@@ -96,12 +100,55 @@ extension GeneralPreferencesViewController: CoreDataManagerDelegate {
     // MARK: - CoreDataManagerDelegate Methods
 
     func newItemInserted(atIndex indexPath: IndexPath) {
-        copyItems = generalPreferencesVM.getCopyItems() ?? []
+        generalPreferencesVM.fetchCopyItems()
         copiedItemsTableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
     }
 
     func itemDeleted(atIndex index: IndexPath) {
-        copyItems = generalPreferencesVM.getCopyItems() ?? []
+        generalPreferencesVM.fetchCopyItems()
         copiedItemsTableView.removeRows(at: IndexSet(integer: index.item), withAnimation: .slideUp)
+    }
+}
+
+extension GeneralPreferencesViewController: NSTextFieldDelegate {
+    // MARK: - NSTextFieldDelegate Methods
+
+    func controlTextDidEndEditing(_ obj: Notification) {
+        guard let storeItemsString = (obj.userInfo?["NSFieldEditor"] as? NSTextView)?.string, let storeItems = Int(storeItemsString), storeItems >= 0 else {
+            numberOfItemToStoreTextField.stringValue = Constants.storeItems.description
+            return
+        }
+
+        if storeItems < generalPreferencesVM.copyItems.count, let window = view.window {
+            alertDialog(withTitle: generalPreferencesVM.getAlertTitle(forStoreItems: storeItems), message: generalPreferencesVM.getAlertMessage(forStoreItems: storeItems)).beginSheetModal(for: window) { response in
+                if response == .alertFirstButtonReturn {
+                    self.generalPreferencesVM.deleteAllItems(fromIndex: storeItems)
+                    self.saveStoreItems(storeItems: storeItems)
+                } else {
+                    self.numberOfItemToStoreTextField.stringValue = Constants.storeItems.description
+                }
+            }
+        } else {
+            saveStoreItems(storeItems: storeItems)
+        }
+    }
+}
+
+extension GeneralPreferencesViewController {
+    // MARK: - Helper Methods
+
+    private func saveStoreItems(storeItems: Int) {
+        Constants.storeItems = storeItems
+        UserDefaults.standard.set(storeItems, forKey: UserDefaultKeys.storeItems)
+    }
+
+    private func alertDialog(withTitle title: String, message: String) -> NSAlert {
+        let alert = NSAlert()
+        alert.messageText = title
+        alert.informativeText = message
+        alert.alertStyle = .critical
+        alert.addButton(withTitle: generalPreferencesVM.alertOkButtonTitle)
+        alert.addButton(withTitle: generalPreferencesVM.alertCancelButtonTitle)
+        return alert
     }
 }
--- Copy Keep/Helpers/Constants.swift
+++ Copy Keep/Helpers/Constants.swift
@@ -12,7 +12,20 @@ struct Constants {
 
     // MARK: - Properties
 
-    static let coreDataModelName = "CopyKeep"
+    static let appName = "Copy Keep"
+
     static let launcherAppId = "com.jaymehta.apps.Auto-Launcher"
     static let killLauncherNotificationName = "killLauncher"
+
+    static let coreDataModelName = "CopyKeep"
+
+    static let defaultStoreItems = 200
+    static var storeItems = defaultStoreItems
+}
+
+struct UserDefaultKeys {
+
+    // MARK: - Properties
+
+    static let storeItems = "storeItems"
 }
